72ab8e5e8f01da18d43f9aea444edec49d96fe08
danutibe07 <danielutibe07@gmail.com>
2023-05-28 11:13:36 +0100

    Added Contacts Routes

--- database/seeders/ContactsTableSeeder.php
+++ database/seeders/ContactsTableSeeder.php
@@ -1,19 +1,62 @@
-<?php
+
+
+<!-- // namespace Database\Seeders;
+
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Seeder;
+// use App\Models\Contact;
+
+// class ContactTableSeeder extends Seeder
+// {
+    /**
+     * Run the database seeds.
+     */
+//     public function run(): void
+//     {
+//         Contact::factory()->count(50)->create();
+//     }
+// } -->
 
 namespace Database\Seeders;
+
+<?php
+
 
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Contact;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+   
 
-class ContactTableSeeder extends Seeder
+class ContactsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Contact::factory()->count(50)->create();
+        DB::table('contacts')->truncate();
+
+        $contacts = [];
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index)
+        {
+            $contacts[] = [
+                'name' => $faker->name(),
+                'address' => $faker->address(),
+                'lastname' =>  $faker->name(),
+                'email' => $faker->email(),
+                'hobbies' => $faker->name(),
+                'company_id'=>Company::factory(),
+                'created_at' => now(),
+                'updated_at' =>  now(),
+            ];
+        }
+
+        DB::table('contacts')->insert($contacts);
     }
 }
+ 
